76cc178f42b6e5f723ca604f504d88fd037ce46a
sagarlama0102 <sagarlama0102@gmail.com>
2026-01-31 00:31:33 +0545

    updated auth.ts axios and endpoints for admin

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -3,5 +3,12 @@ export const API = {
     AUTH: {
         LOGIN: '/api/auth/login',
         REGISTER: '/api/auth/register',
+        WHOAMI: '/api/auth/whoami',
+        UPDATEPROFILE: '/api/auth/update-profile',
+    },
+    ADMIN: {
+        USER: {
+            CREATE: '/api/admin/users/',
+        }
     }
 }
