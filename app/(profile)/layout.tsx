2d388ca62b05a7691dc3727844f12c66b5578674
sagarlama0102 <sagarlama0102@gmail.com>
2026-01-31 00:09:36 +0545

    set up proxy.ts

---
+++ app/(profile)/layout.tsx
@@ -0,0 +1,10 @@
+import Header from "@/app/(public)/_components/Header"; // Adjust this path to your public Header
+import Footer from "@/app/(public)/_components/Footer";
+
+export default function ProfileLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return 
+}
