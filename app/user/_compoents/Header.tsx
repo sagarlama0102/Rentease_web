8d2344b9263a524e9a60edbb63a2d2fdf31193ce
sagarlama0102 <sagarlama0102@gmail.com>
2026-02-08 00:52:54 +0545

    implemented updateUserForm and userTable to display

--- app/user/_compoents/Header.tsx
+++ app/user/_compoents/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-// import ThemeToggle from "@/app/_components/ThemeToggle";
+import ThemeToggle from "@/app/_components/ThemeToggle";
 import { handleLogout } from "@/lib/actions/auth-action";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
