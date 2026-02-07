6c6fdcc47789224cc5a53b1816b3a243d76ac9c2
sagarlama0102 <sagarlama0102@gmail.com>
2026-02-08 00:40:28 +0545

    implemented getall users, delete, update etc from admin

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getAuthToken, getUserData } from "./lib/cookie";
 
-const publicRoutes = ['/login', '/register'];
+const publicRoutes = ['/login', '/register', '/forget-password', '/reset-password'];
 const adminRoutes = ['/admin'];
 const userRoutes = ['/user'];
 
